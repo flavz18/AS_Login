c860136702d1829467308c58b2e45501302c42a5
bagusSaptoaji <bagussaptoaji@gmail.com>
2023-06-26 10:52:26 +0700

    update

--- app/src/main/java/com/bagoes/login/MainActivity.java
+++ app/src/main/java/com/bagoes/login/MainActivity.java
@@ -40,34 +40,31 @@ public class MainActivity extends AppCompatActivity
 
                 _id = _idEditText.getText().toString();
                 _password = _passwordEditText.getText().toString();
-
-                _url = "http://tonywijaya.000webhostapp.com/011100862/login.php?id=" + "&password" + _password;
+                _url = "https://stmikpontianak.net/011100862/login.php?id=" + _id + "&password=" + _password;
 
                 asyncHttpClient = new AsyncHttpClient();
                 asyncHttpClient.get(_url, new AsyncHttpResponseHandler() {
                     @Override
-                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody)
-                    {
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                         String hasil = new String(responseBody);
+//                        Toast.makeText(getApplicationContext(), new String(responseBody), Toast.LENGTH_SHORT).show();
 
-                        if (!hasil.equals("[{\"idCount\":\"1\"}]"))
-                        {
-                            Toast.makeText(getApplicationContext(), "ID dan Password anda tidak cocok.", Toast.LENGTH_LONG).show();
+                        if (!hasil.equals("[{\"idCount\":\"1\"}]")) {
+                            Toast.makeText(getApplicationContext(), "ID dan Password anda tidak cocok.", Toast.LENGTH_SHORT).show();
                             return;
                         }
 
                         Toast.makeText(getApplicationContext(), "Selamat datang, " + _id, Toast.LENGTH_SHORT).show();
-
                         _menuIntent = new Intent(getApplicationContext(), MenuActivity.class);
                         startActivity(_menuIntent);
                     }
 
                     @Override
-                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
-                    {
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                         Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
+
             }
         });
     }
